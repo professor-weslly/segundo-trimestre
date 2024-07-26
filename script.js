1b38848598d0b0fbc88c03994f4d863f902cf79d
fernanda1417 <135229119+fernanda1417@users.noreply.github.com>
2024-07-26 07:47:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,132 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+cost caixaPrincipal=document.querySelector(".caixa-principal")
+cost caixaPerguntas=document.querySelector(".caixa-perguntas")
+cost caixaAlternativas=document.querySelector(".caixa-alternativas")
+cost caixaResultado=document.querySelector(".caixa-resultado")
+cost textoResultado=document.querySelector(".texto-resultado")
 
-const perguntas = [
+cost perguntas=[
     {
-        enunciado: "Como é sua noite de sono?,
-            alternativas: [
-            {   
-            texto:"Boa",
-            afirmação: "afirmação",
-            },
+        enunciado:"Você costuma fumar",
+        alternativas:[
             {
-                texto:"Ruim",
-                afirmação:"afirmação",
-            },
-        ]
-
-    {
-        enunciado: "Sua alimentação é saudavel?",
-        alternativas: [
-            {   
-                texto:"Sim",
-                afirmação: "afirmação",
-                },
-                {
-                    texto:"Não",
+                texto:"Sim"
                     afirmação:"afirmação",
-                },
-            ]
-    },
-
+    }
     {
-        enunciado: "Qual é a duração do seu sono?",
-        alternativas: [
-            {   
-                texto:"Mais de 8 horas",
-                afirmação: "afirmação",
-                },
-                {
-                    texto:"Menos de 8 horas",
+        texto:"Não"
+            afirmação:"afirmação"
+}
+]  
+    },    
+      {
+       enunciado:"Você se exercita?",
+        alternativas:[
+           {
+                texto:"Sim"
                     afirmação:"afirmação",
-                },
-            ]
-    },
-
+    }
     {
-        enunciado: "Qual sua cor preferida?",
-        alternativas: [
-            {   
-                texto:"Azul",
-                afirmação: "afirmação",
-                },
-                {
-                    texto:"Rosa",
+        texto:"Não"
+            afirmação:"afirmação"
+ {
+     enunciado:"Você gosta de estudar?",
+    alternativas:[
+ {
+                texto:"Sim"
                     afirmação:"afirmação",
-                },
-            ]
-    },
+    }
     {
-        enunciado: "Quantos litros de agua você?",
-        alternativas: [
-            {   
-                texto:"Mais de 3 litros ",
-                afirmação: "afirmação",
-                },
-                {
-                    texto:"Menos de 3 litros ",
+        texto:"Não"
+            afirmação:"afirmação"
+}    
+  {
+     enunciado:"Você é mais de exatas ou humanas?",
+    alternativas:[
+ {
+                texto:"Exatas"
                     afirmação:"afirmação",
-                },
-        ]
-    },
-];
+    }
+    {
+        texto:"Humanas"
+            afirmação:"afirmação"
+}
+  {
+     enunciado:"O que você prefere?",
+    alternativas:[
+      {
+                texto:"Doce"
+                    afirmação:"afirmação",
+    {
+        texto:"Salgado"
+            afirmação:"afirmação"
+}
+  },  
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal ="";
 
 function mostraPergunta() {
-if(atual >= perguntas.length){
-mostrarResultado();
-return;
-}
+    if(atual >= perguntas.lenght){
+    mostraResultados();
+        return;
+         }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
+    caixaAlternativas.textoContent=""
     mostraAlternativas();
 }
 
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",()> respostaSelecionada(alternativa));   
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-function respostaSelecionada(opcaoSelecinada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-historiaFinal += afirmacoes + "";
+
+mostraPergunta();
+         }
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent ="";
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
-    
-mostraPergunta
+mostraPergunta();
+function mostraResultado(){
+    caixaPerguntas.textC = "";
+}
+}
+mostraPergunta()
